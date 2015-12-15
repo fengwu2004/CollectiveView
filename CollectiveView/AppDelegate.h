17d2a6ab5488d32aff5151bd3a7371447f3e4422
fengwu2004 <user@users-imac-2.local>
2015-12-15 15:20:52 +0800

    Initial Commit

---
+++ CollectiveView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollectiveView
+//
+//  Created by user on 15/12/15.
+//  Copyright © 2015年 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
