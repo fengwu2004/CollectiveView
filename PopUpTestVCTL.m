48816a7f44688e4d7b67ab9bafe86c0638c28202
fengwu2004 <user@users-iMac-2.local>
2016-01-15 18:20:16 +0800

    anim

--- PopUpTestVCTL.m
+++ PopUpTestVCTL.m
@@ -23,7 +23,7 @@
 
 - (IBAction)onPopUpBox:(id)sender {
 	
-	PopUpBoxItem *item0 = [PopUpBoxItem makePopItem:@"giftn11" title:@"秀场的成员管理" block:nil];
+	PopUpBoxItem *item0 = [PopUpBoxItem makePopItem:@"giftn11" title:@"秀场管理" block:nil];
 	
 	PopUpBoxItem *item1 = [PopUpBoxItem makePopItem:@"giftn12" title:@"秀场名称" block:nil];
 	
@@ -41,9 +41,29 @@
 	
 	CGPoint point = button.frame.origin;
 	
-	PopUpBox *box = [[PopUpBox alloc] initWithFrame:CGRectMake(point.x, point.y + 50, 120, 30 * 6) with:array];
+	point.y += 50;
+	
+	PopUpBox *box = [[PopUpBox alloc] initWithFrame:CGRectMake(point.x, point.y, 120, 30 * 6) with:array];
+	
+	CGRect OrignalRect = box.bounds;
+	
+	CGRect minRect = CGRectMake(0, 0, 1, 1);
+	
+	box.bounds = minRect;
+	
+	box.center = point;
+	
+	box.clipsToBounds = YES;
 	
 	[self.view addSubview:box];
+	
+	[UIView animateWithDuration:0.5 delay:0 usingSpringWithDamping:1 initialSpringVelocity:5.0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+		
+		box.center = CGPointMake(point.x + 60, point.y + 90);
+		
+		box.bounds = OrignalRect;
+		
+	} completion:nil];
 }
 
 @end
